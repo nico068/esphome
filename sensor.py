39a02cc084709007391725040acc735944f786c1
nico068 <144681757+nico068@users.noreply.github.com>
2025-03-28 09:46:42 +0100

    sensor.py

---
+++ sensor.py
@@ -0,0 +1,89 @@
+import esphome.codegen as cg
+from esphome.components import i2c, sensor
+import esphome.config_validation as cv
+from esphome.const import (
+    CONF_ID,
+    CONF_TEMPERATURE,
+    DEVICE_CLASS_TEMPERATURE,
+    ICON_BRIEFCASE_DOWNLOAD,
+    ICON_SCREEN_ROTATION,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_CELSIUS,
+    UNIT_DEGREE_PER_SECOND,
+    UNIT_METER_PER_SECOND_SQUARED,
+)
+
+DEPENDENCIES = ["i2c"]
+
+CONF_ACCEL_X = "accel_x"
+CONF_ACCEL_Y = "accel_y"
+CONF_ACCEL_Z = "accel_z"
+CONF_GYRO_X = "gyro_x"
+CONF_GYRO_Y = "gyro_y"
+CONF_GYRO_Z = "gyro_z"
+CONF_MAG_X = "mag_x"
+CONF_MAG_Y = "mag_y"
+CONF_MAG_Z = "mag_z"
+
+mpu9250_ns = cg.esphome_ns.namespace("mpu9250")
+MPU9250Component = mpu9250_ns.class_(
+    "MPU9250Component", cg.PollingComponent, i2c.I2CDevice
+)
+
+accel_schema = sensor.sensor_schema(
+    unit_of_measurement=UNIT_METER_PER_SECOND_SQUARED,
+    icon=ICON_BRIEFCASE_DOWNLOAD,
+    accuracy_decimals=2,
+    state_class=STATE_CLASS_MEASUREMENT,
+)
+gyro_schema = sensor.sensor_schema(
+    unit_of_measurement=UNIT_DEGREE_PER_SECOND,
+    icon=ICON_SCREEN_ROTATION,
+    accuracy_decimals=2,
+    state_class=STATE_CLASS_MEASUREMENT,
+)
+mag_schema = sensor.sensor_schema(
+    unit_of_measurement=UNIT_MICROTESLA,
+    icon=ICON_MAGNET,
+    accuracy_decimals=4,
+    state_class=STATE_CLASS_MEASUREMENT,
+)
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(MPU9250Component),
+            cv.Optional(CONF_ACCEL_X): accel_schema,
+            cv.Optional(CONF_ACCEL_Y): accel_schema,
+            cv.Optional(CONF_ACCEL_Z): accel_schema,
+            cv.Optional(CONF_GYRO_X): gyro_schema,
+            cv.Optional(CONF_GYRO_Y): gyro_schema,
+            cv.Optional(CONF_GYRO_Z): gyro_schema,
+            cv.Optional(CONF_MAG_X): mag_schema,
+            cv.Optional(CONF_MAG_Y): mag_schema,
+            cv.Optional(CONF_MAG_Z): mag_schema,
+        }
+    )
+    .extend(cv.polling_component_schema("10s"))
+    .extend(i2c.i2c_device_schema(0x68))
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
+
+    for d in ["x", "y", "z"]:
+        accel_key = f"accel_{d}"
+        if accel_key in config:
+            sens = await sensor.new_sensor(config[accel_key])
+            cg.add(getattr(var, f"set_accel_{d}_sensor")(sens))
+        accel_key = f"gyro_{d}"
+        if accel_key in config:
+            sens = await sensor.new_sensor(config[accel_key])
+            cg.add(getattr(var, f"set_gyro_{d}_sensor")(sens))
+        accel_key = f"mag_{d}"
+        if accel_key in config:
+            sens = await sensor.new_sensor(config[accel_key])
+            cg.add(getattr(var, f"set_mag_{d}_sensor")(sens))
